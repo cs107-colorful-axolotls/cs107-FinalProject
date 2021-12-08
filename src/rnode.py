74c001a140543d9b12dbbad49313409f1e76fb18
jeremyzhang1 <zhang.jeremy.2001@gmail.com>
2021-12-08 11:30:17 -0500

    add dunders

--- src/rnode.py
+++ src/rnode.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.lib.arraysetops import isin
 
 
 class Rnode:
@@ -72,3 +73,73 @@ class Rnode:
         z = Rnode(-self._val)
         self._children.append((-1, z))
         return z
+
+
+    def transform(self, a, b):
+        if isinstance(b, float) or isinstance(b, int):
+            b = Rnode([b] * len(a._val))
+
+        return b
+
+
+    def __add__(self, other):
+        other = self.transform(self, other)
+        z = Rnode(self._val + other._val)
+        self._children.append((np.ones(self._val.shape), z))
+        other._children.append((np.ones(self._val.shape), z))
+        return z
+
+
+    def __radd__(self, other):
+        return self + other
+
+
+    def __sub__(self, other):
+        other = self.transform(self, other)
+        z = Rnode(self._val - other._val)
+        self._children.append((np.ones(self._val.shape), z))
+        other._children.append((np.ones(self._val.shape), z))
+        return z
+
+
+    def __rsub__(self, other):
+        return self - other
+
+        
+    def __mul__(self, other):
+        """
+        Overloads multiplication
+
+        Parameters:
+        other: Value or Rnode that represents the amount to multiply the current Rnode or value by
+
+        Returns:
+        For Fnodes, a new Rnode object where the self and other Rnodes are multiplied according to the product rule
+        For values, a new Rnode object where the self and other values are multiplied according to the product rule
+        """
+        self.transform(self, other)
+        z = Rnode(self._val * other._val)
+        self._children.append((other._val, z))
+        other._children.append((self._val, z))
+
+    
+    def __rmul__(self, other):
+        return self * other
+
+
+    def __truediv__(self, other):
+        """
+        Overloads division
+
+        Parameters:
+        other: Value or Rnode that represents the amount to divide the current Rnode or value by
+
+        Returns:
+        For Fnodes, a new Rnode object where the self and other Rnodes are divided according to the quotient rule
+        For values, a new Rnode object where the self and other values are divided according to the quotient rule
+        """
+        return self * (other ** (-1))
+
+
+    def __rtruediv__(self, other):
+        return other * (self ** (-1))
