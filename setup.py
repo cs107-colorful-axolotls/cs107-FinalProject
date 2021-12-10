f26c87883df86077b277a3b2dfb2f530e38cd906
Fiona Henry <fhenry@g.harvard.edu>
2021-12-10 11:21:49 -0500

    working on packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="auto_diff",
+    version="1.0.0",
+    description="A package for forward and reverse automatic differentiation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cs107-colorful-axolotls/cs107-FinalProject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
